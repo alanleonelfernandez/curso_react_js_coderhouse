d3163ed0c458753b910cdadf778ca97e3b2fabf5
Alan Fernandez <alanleonelfernandez@hotmail.com>
2021-11-03 11:54:00 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from 'react';
+import './App.css';
+import Header from './components/Header/Header'
+
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+} 
+
+
+
+export default App;
+
+/* const App = () => {
+  return(
+    <h1>Ahora soy un arrow</h1>
+  )
+} */
+
+//JSX -> JavaScript XML (Javascript + HTML)
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+import './header.css'
+
+const Header = () => {
+    return(
+        <header>
+            <nav>
+                <h1>Hola Mundo!</h1>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
